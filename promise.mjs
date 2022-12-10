98fefe3ebb125f2ab7e726d222b362a391b5187d
obaidmuneer <obaidmuneer55@gmail.com>
2022-12-10 20:13:34 +0500

    converted callback hell to promises

--- promise.mjs
+++ promise.mjs
@@ -3,16 +3,70 @@
 // it can either result in a value or error
 ///promise basicaly promises you that it will give you the result of an asynchrous operation
 
-let random = Math.random().toFixed();
-let p = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    if (random > 0) {
-      resolve(1);
-    } else {
-      reject(new Error("message"))
-    }
-  }, 2000);
-});
+// let random = Math.random().toFixed();
+// let p = new Promise((resolve, reject) => {
+//   //kickoff some async work
+//   setTimeout(() => {
+//     if (random > 0) {
+//       resolve(1);
+//     } else {
+//       reject(new Error("message"));
+//     }
+//   }, 2000);
+// });
 
-p.then((res) => console.log(res));
-p.catch((err) => console.log(err.message));
+// p.then((res) => console.log(res));
+// p.catch((err) => console.log(err.message));
+
+// converting callback hell to promises
+console.log("Before Func Call");
+
+getUser(1)
+  .then((res) => getRepo(res.githubUser))
+  .then((res) => getCommit(res[0].repo[0]))
+  .then((res) => console.log(res))
+  .catch((err) => console.log(err.message));
+
+console.log("After Func Call");
+
+function getUser(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Getting User");
+      resolve({
+        id,
+        githubUser: "obaid",
+      });
+    }, 2000);
+  });
+}
+
+function getRepo(user) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Getting Repos");
+      resolve([
+        {
+          githubUser: user,
+          repo: ["repo1", "repo2", "repo3"],
+        },
+      ]);
+    }, 2000);
+  });
+}
+
+function getCommits(repo) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Getting Commits");
+      resolve([
+        {
+          repo,
+          commit: ["commit1", "commit2", "commit3"],
+        },
+      ]);
+    }, 2000);
+  });
+}
+
+export { getUser, getRepo, getCommits };
